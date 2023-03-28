724be2ba520a490aebbb8b13f1269ee844f40279
acer <1027517802@qq.com>
2023-03-28 23:10:19 +0800

    接口

--- day3/interface.go
+++ day3/interface.go
@@ -57,6 +57,23 @@ func (d dog) say() {
 func (c cat) say() {
 	fmt.Println("喵喵喵")
 }
+
+// ----------------------------
+type People interface {
+	Speak(string) string
+}
+
+type Student struct{}
+
+func (stu *Student) Speak(think string) (talk string) {
+	if think == "sb" {
+		talk = "你是个大帅比"
+	} else {
+		talk = "您好"
+	}
+	return
+}
+
 func main() {
 	var x Sayer // 声明一个Sayer类型的变量x
 	a := cat{}  // 实例化一个cat
@@ -65,4 +82,9 @@ func main() {
 	x.say()     // 喵喵喵
 	x = b       // 可以把dog实例直接赋值给x
 	x.say()     // 汪汪汪
+
+	fmt.Println("-----------------------------")
+	var peo People = &Student{}
+	think := "bitch"
+	fmt.Println(peo.Speak(think))
 }
